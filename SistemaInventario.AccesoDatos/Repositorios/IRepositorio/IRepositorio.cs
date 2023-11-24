191a32aa259bdd2da966b491e16202378ba78657
Alexander Martinez Moron <alex.martinezmoron@gmail.com>
2023-11-24 12:50:19 +0100

    repositoriosp

---
+++ SistemaInventario.AccesoDatos/Repositorios/IRepositorio/IRepositorio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorios.IRepositorio
+{
+    public interface IRepositorio<T> where T : class
+    {
+        Task<T> Obtener(int id);
+
+        Task<IEnumerable<T>> ObtenerTodos(
+            Expression<Func<T, bool>> filtro =null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string incluirPropiedades = null,
+            bool isTraking = true);
+
+        Task<T> ObtenerPrimero(
+               Expression<Func<T, bool>> filtro = null,
+            
+            string incluirPropiedades = null,
+            bool isTraking = true);
+
+        Task Agregar(T entidad);
+
+        void Remover(T entidad);
+        void RemoverRango(IQueryable<T> entidad);
+            
+    }
+}
---
+++ SistemaInventario.AccesoDatos/Repositorios/IRepositorio/Repositorio.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaInventario.AccesoDatos.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorios.IRepositorio
+{
+    public class Repositorio<T> : IRepositorio<T> where T : class
+    {
+
+        public readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repositorio(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+
+        public async Task Agregar(T entidad)
+        {
+           await dbSet.AddAsync(entidad);   // intert into Table
+        }
+
+        public async Task<T> Obtener(int id)
+        {
+            return await dbSet.FindAsync(id);   // select * from (Solo por Id)
+        }
+
+        public async Task<IEnumerable<T>> ObtenerTodos(Expression<Func<T, bool>> filtro = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string incluirPropiedades = null, bool isTraking = true)
+        {
+            IQueryable<T> query = dbSet;
+            if (filtro != null)
+            {
+                query = query.Where(filtro);  // select * from where ....
+            }
+            if (incluirPropiedades != null)
+            {
+                foreach (var incluirProp in incluirPropiedades.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(incluirProp);  // ejemplo "Categorio,Marca"
+                }
+
+            }
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+            if (!isTraking)
+            {
+                query = query.AsNoTracking();           
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> ObtenerPrimero(Expression<Func<T, bool>> filtro = null, string incluirPropiedades = null, bool isTraking = true)
+        {
+            IQueryable<T> query = dbSet;
+            if (filtro != null)
+            {
+                query = query.Where(filtro);  // select * from where ....
+            }
+            if (incluirPropiedades != null)
+            {
+                foreach (var incluirProp in incluirPropiedades.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(incluirProp);  // ejemplo "Categorio,Marca"
+                }
+
+            }
+            
+            if (!isTraking)
+            {
+                query = query.AsNoTracking();
+            }
+
+            return await query.FirstOrDefaultAsync();
+        }
+              
+
+        public void Remover(T entidad)
+        {
+            dbSet.Remove(entidad);
+        }
+
+        public void RemoverRango(IQueryable<T> entidad)
+        {
+            dbSet.RemoveRange();
+        }
+    }
+}
