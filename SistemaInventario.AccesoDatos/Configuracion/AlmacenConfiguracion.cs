2500a85d647f72cc8f54a90639090240863df221
Alexander Martinez Moron <alex.martinezmoron@gmail.com>
2023-11-24 11:30:11 +0100

    inicial

---
+++ SistemaInventario.AccesoDatos/Configuracion/AlmacenConfiguracion.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using SistemaInventario.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Configuracion
+{
+    public class AlmacenConfiguracion : IEntityTypeConfiguration<Almacen>
+    {
+        public void Configure(EntityTypeBuilder<Almacen> builder)
+        {
+            builder.Property(x => x.Id).IsRequired();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(60);
+            builder.Property(x => x.Description).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Estado).IsRequired();
+
+        }
+    }
+}
---
+++ SistemaInventario.Modelos/Almacen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Almacen
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Nombre es Requerido")]
+        [MaxLength(60, ErrorMessage ="Nombre debe ser MAximo 60 caracteres")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Descripcion es Requerido")]
+        [MaxLength(100, ErrorMessage = "La descripcion debe ser MAximo 100 caracteres")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Estado requerido")]
+        public bool Estado { get; set; }
+    }
+}
